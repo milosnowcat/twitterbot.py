74b77ff8c644d7412091fd477841c84db6a8fdbe
milosnowcat <ramiro.alvarez@rahcode.com>
2023-10-05 21:32:45 -0600

    comments

--- main.py
+++ main.py
@@ -50,6 +50,12 @@ from dotenv import load_dotenv
 import utils
 
 def api():
+    """
+    The `api()` function returns an instance of the `tweepy.API` class with the necessary authentication
+    credentials.
+    :return: The function `api()` returns an instance of the `tweepy.API` class, which is authenticated
+    using the provided API key, API secret, access token, and access token secret.
+    """
     load_dotenv()
     
     api_key = os.environ.get('Api_Key')
@@ -67,10 +73,25 @@ def api():
     return tweepy.API(auth)
 
 def tweet(api: tweepy.API, message: str):
+    """
+    The function `tweet` takes in an instance of the `tweepy.API` class and a string `message`, and uses
+    the API to update the user's status with the given message.
+    
+    :param api: The `api` parameter is an instance of the `tweepy.API` class. It is used to interact
+    with the Twitter API and perform actions such as updating the status (tweeting)
+    :type api: tweepy.API
+    :param message: The message parameter is a string that represents the content of the tweet you want
+    to post
+    :type message: str
+    """
     api.update_status(message)
     print('Tweeted successfully! ')
 
 def main():
+    """
+    The main function retrieves a quote and its author using the utils module, and then tweets the quote
+    and author using the api module.
+    """
     quote, author = utils.get_quote()
     tweet(api(), f'{quote} ~ {author}')
 
